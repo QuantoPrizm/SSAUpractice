643cee92d7c39c637856e704c93608288640cd35
QuantoPrizm <qweasdzxcqazwsxedc16@mail.ru>
2025-06-27 13:42:18 +0400

    перенос пароля в приватный файл

--- BoardGames/src/main/java/ru/ssau/BoardGames/BoardGamesApplication.java
+++ BoardGames/src/main/java/ru/ssau/BoardGames/BoardGamesApplication.java
@@ -2,12 +2,16 @@ package ru.ssau.BoardGames;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+		@PropertySource("classpath:application.properties"),
+		@PropertySource("classpath:privateApplication.properties")
+})
 public class BoardGamesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BoardGamesApplication.class, args);
 	}
-
 }
